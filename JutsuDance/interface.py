f71067b308c91702f1592ecd4cae3c2a2001ccd2
ComteHerrapait <leondecheux@gmail.com>
2020-04-03 14:37:07 +0200

    interface created
    il faut bien installer les librairies necessaires
    
    pip install pyqt5

---
+++ JutsuDance/interface.py
@@ -0,0 +1,17 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+
+
+class Interface(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        uic.loadUi("interface.ui", self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Interface()
+    window.show()
+    sys.exit(app.exec_())
