3cbd446b520ff41fd9f2bfe2ff9ef606d0bbe087
ComteHerrapait <leondecheux@gmail.com>
2020-04-07 17:48:52 +0200

    réorganisation

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ from PyQt5 import uic
 import sys
 
 from acquisition import *
-#exec("interaction.py")
 
 
 class Interface(QtWidgets.QMainWindow):
@@ -13,25 +12,33 @@ class Interface(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         uic.loadUi("interface.ui", self)
 
+        #timer responsable du rafraichissement de l'interface
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.update_camera)
+        self.timer.timeout.connect(self.update)
         self.timer.start(30)
 
     def acquisition(self):
+        """ recupère une image depuis la camera avec le module acquisition"""
         return acq("RGB")
 
-    def update_camera(self):
+    def update(self):
+        """ mets à jour l'affichage sur l'interface"""
+        #affiche le flux de la caméra
         frame = self.acquisition()
-
         image = QtGui.QImage(frame, frame.shape[1], frame.shape[0],
                        frame.strides[0], QtGui.QImage.Format_RGB888)
         self.label.setPixmap(QtGui.QPixmap.fromImage(image))
 
 if __name__ == "__main__":
+    """Fonction MAIN """
+
     app = QtWidgets.QApplication(sys.argv)
     window = Interface()
     window.show()
     sys.exit(app.exec_())
 
-#   lien vers un exemple pour afficher une image
-#   https://gist.github.com/bsdnoobz/8464000
+
+
+
+    #   lien vers un exemple pour afficher une image
+    #   https://gist.github.com/bsdnoobz/8464000
