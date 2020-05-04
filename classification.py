fa73a73edca58c87f7e699462effd499c1275de1
x3vsx2 <44469280+x3vsx2@users.noreply.github.com>
2020-05-04 15:26:35 +0200

    Meanshist fonctionnel
    Segementation par meanshift fonctionnel : petite image 64*64 sinon tps d'exécution explose

--- classification.py
+++ classification.py
@@ -22,29 +22,34 @@ def segmenatationMain(image):
     """"Fonction qui binarise l'image de la main
     input : image centrée sur la main 
     output : image binaire d la main """
-    if np.size(image)!=(512,512):
-        image=cv2.resize(image,(512,512))
+    taille=(64,64)
+    if np.size(image)!=taille:
+        image=cv2.resize(image,taille)
     imLab=cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
-    ms=MeanShift(0.2)
-    ms.fit(imLab[:,:,1])
+    plana=np.reshape(imLab[:,:,1],taille[1]*taille[0])
+    planb=np.reshape(imLab[:,:,2],taille[1]*taille[0])
+    data=np.concatenate((np.atleast_2d(plana).T,np.atleast_2d(planb).T),axis=1)  
+    bdw=10
+    ms=MeanShift(bdw)
+    ms.fit(data)
     lblsAll=ms.labels_ 
-    cluster_centers=ms.cluster_centers_ #calcule les centres des classes
-    lbls=np.unique(lblsAll) #supprime les doublons
-    k=len(lbls) #nombre de classe
-    return(lblsAll,k,cluster_centers)
+    #cluster_centers=ms.cluster_centers_ #calcule les centres des classes
+    #lbls=np.unique(lblsAll) #supprime les doublons
+    #k=len(lbls) #nombre de classe
+    image_seg=np.reshape(lblsAll,taille)
+    return(image_seg)
 
 image=cv2.imread('7.jpg')
-#cv2.namedWindow('Input')
-#cv2.resizeWindow('Input',512,512)
-#image=cv2.resize(image,(512,512))
-#cv2.imshow("Input",image)
-#imseg,k,centers=segmenatationMain(image)
-#cv2.imshow('Input',imseg)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+# cv2.namedWindow('Input')
+# cv2.resizeWindow('Input',512,512)
+# image=cv2.resize(image,(512,512))
+# cv2.imshow("Input",image)
+# imseg=segmenatationMain(image)
+# cv2.imshow('Input',imseg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-plt.imread('7.jpg')
-imseg,k,centers=segmenatationMain(image)
+imseg=segmenatationMain(image)
 plt.figure();
 plt.imshow(imseg)
 plt.show()
