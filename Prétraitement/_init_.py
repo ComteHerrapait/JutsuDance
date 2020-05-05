eb730b673ea9fe09b43d9301e630f74548d7e610
Arthur Boiron <41791422+Mousquitaire@users.noreply.github.com>
2020-05-05 11:12:30 +0200

    Création du dossier Prétraitement

---
+++ Prétraitement/_init_.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 10:10:18 2020
+
+@author: Arthu
+"""
+
+
--- Test_Mean_shift.py
+++ Test_Mean_shift.py
@@ -6,17 +6,30 @@ Created on Tue Apr  7 17:03:52 2020
 """
 
 import numpy as np 
-import cv2 
+import cv2
+import matplotlib.pyplot as plt
 
   
 # read video 
 cap = cv2.VideoCapture(0) 
-   
 # retrieve the very first  
 # frame from the video
-fond = cv2.imread("./fond.jpg")
+#fond = cv2.imread("./fond.jpg")
 _, frame = cap.read()
 #frame = cv2.subtract(fond,frame)
+main = cv2.imread("./Image_main.jpg")
+main_hsv = cv2.cvtColor(main, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(main_hsv, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+roi1 = cv2.calcHist([main_hsv], [0], mask, [180], [0, 180])
+roi2 = cv2.calcHist([main_hsv], [1], mask, [180], [0, 180])
+roi3 = cv2.calcHist([main_hsv], [2], mask, [180], [0, 180])
+cv2.imshow('main', main_hsv)
+plt.hist(roi1)
+plt.show()
+plt.hist(roi2)
+plt.show()
+plt.hist(roi3)
+plt.show()
 
    
 # set the region for the 
@@ -33,10 +46,11 @@ r_o_i = frame[p:p + q, r:r + s]
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
    
 # apply mask on the HSV frame 
-mask = cv2.inRange(hsv, np.array((0., 80., 33.)), np.array((180., 255., 255.))) 
+mask = cv2.inRange(hsv, np.array((0., 60.,32.)), np.array((180.,255.,255.))) 
   
 # get histogram for hsv channel 
-roi = cv2.calcHist([hsv], [0], mask, [180], [0, 180]) 
+roi = cv2.calcHist([hsv], [1], mask, [180], [0, 180])
+#plt.hist(roi)
   
 # normalize the retrieved values 
 cv2.normalize(roi, roi, 0, 255, cv2.NORM_MINMAX) 
@@ -46,17 +60,17 @@ cv2.normalize(roi, roi, 0, 255, cv2.NORM_MINMAX)
 termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 15, 2 ) 
 
 while(True): 
-    _, frame = cap.read() 
+    _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     #frame = cv2.subtract(fond,frame)
    
     frame = cv2.resize(frame, (1280, 720), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC) 
-   
+    
     # convert BGR to HSV format 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     cv2.imshow('hsv', hsv)
       
-    bp = cv2.calcBackProject([hsv], [0], roi, [0, 180], 1) 
+    bp = cv2.calcBackProject([hsv], [1], roi, [0, 180], 1) 
    
     # applying meanshift to get the new region 
     #_, track_window = cv2.meanShift(bp, track_window, termination)
