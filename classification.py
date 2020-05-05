9e5e601ffff5922b311cd7ab2f3ceb2d368c9966
x3vsx2 <44469280+x3vsx2@users.noreply.github.com>
2020-05-05 09:49:07 +0200

    clean up

--- classification.py
+++ classification.py
@@ -4,19 +4,7 @@ from sklearn.cluster import MeanShift
 import matplotlib.pyplot as plt
 
 
-def classifier(image,classes):
-    """Retourne la classe de l'image en paramètre"""
-    FeatureVector=featureCalculator(image)
-    les_distances=np.zeros(1,len(classes))
-    
-    
-
-    
-
-def featureCalculator(image):
-    """Calcule le vecteur caractèristique de l'image"""
-    return(np.zeros(5))
-    
+     
 
 def segmenatationMain(image):
     """"Fonction qui binarise l'image de la main
