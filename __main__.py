79f00488facdf279bb8e1e1e4c10d2f4f7b8a1ba
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2020-05-05 11:17:35 +0200

    commentaires et fonction display

--- __main__.py
+++ __main__.py
@@ -12,6 +12,7 @@ class Interface(QtWidgets.QMainWindow):
     CameraIndex = 0
 
     def __init__(self):
+        """constrcteur"""
         QtWidgets.QMainWindow.__init__(self)
         uic.loadUi("interface.ui", self)
 
@@ -25,23 +26,30 @@ class Interface(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.update)
         self.timer.start(30)
 
+    def display(self,image):
+        """affiche une image dans le label au centre de l'interface"""
+        image = QtGui.QImage(image, image.shape[1], image.shape[0],
+                       image.strides[0], QtGui.QImage.Format_RGB888)
+        self.label.setPixmap(QtGui.QPixmap.fromImage(image))
+
     def update(self):
         """ mets à jour l'affichage sur l'interface"""
         #affiche le flux de la caméra
         frame = self.camera.acq("RGB")
-        image = QtGui.QImage(frame, frame.shape[1], frame.shape[0],
-                       frame.strides[0], QtGui.QImage.Format_RGB888)
-        self.label.setPixmap(QtGui.QPixmap.fromImage(image))
+        self.display(frame)
 
     def __del__(self):
+        """destructeur"""
         cv2.destroyAllWindows()
 
     def plus(self):
+        """camera suivante"""
         if self.camera.changeInput(self.CameraIndex + 1):
             self.CameraIndex +=1
             self.labelCamera.setText(str(self.CameraIndex))
 
     def minus(self):
+        """camera précédente"""
         if self.camera.changeInput(self.CameraIndex - 1):
             self.CameraIndex -=1
             self.labelCamera.setText(str(self.CameraIndex))
