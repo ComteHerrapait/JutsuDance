84cf079bacc1c4ab33501e5ae6227d8033c8e961
Arthur Boiron <41791422+Mousquitaire@users.noreply.github.com>
2020-05-06 11:28:26 +0200

    Modification de _init_.py + modification du nom du package

--- pretraitement/_init_.py
+++ pretraitement/_init_.py
@@ -8,7 +8,7 @@ Created on Tue May  5 10:10:18 2020
 import numpy as np 
 import cv2
 
-def Pretraitement():
+def pretraitement():
     # read video 
     cap = cv2.VideoCapture(0) 
     # retrieve the very first  
@@ -43,29 +43,31 @@ def Pretraitement():
     # iteration or by at least 2 pt 
     termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 15, 2 ) 
     
-    
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    frame = cv2.resize(frame, (1280, 720), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC) 
-    
-    # convert BGR to HSV format 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-      
-    bp = cv2.calcBackProject([hsv], [1], roi, [0, 180], 1) 
-       
-    # applying meanshift to get the new region 
-    #_, track_window = cv2.meanShift(bp, track_window, termination)
-    ret, track_window = cv2.CamShift(bp, track_window, termination) 
-       
-    # Draw track window on the frame 
-    #x, y, w, h = track_window 
-    #img2 = cv2.rectangle(frame, (x, y), (x + w*2, y + h*2), 255, 2) 
-    pts = cv2.boxPoints(ret)
-    pts = np.int0(pts)
-    img2 = cv2.polylines(frame,[pts],True, 255,2)
-      
-    # show results 
-    #cv2.imshow('tracker', img2)
+    i = 0
+    while(i<50):
+        i = i+1
+        _, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        frame = cv2.resize(frame, (1280, 720), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC) 
+        
+        # convert BGR to HSV format 
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+          
+        bp = cv2.calcBackProject([hsv], [1], roi, [0, 180], 1) 
+           
+        # applying meanshift to get the new region 
+        #_, track_window = cv2.meanShift(bp, track_window, termination)
+        ret, track_window = cv2.CamShift(bp, track_window, termination) 
+           
+        # Draw track window on the frame 
+        #x, y, w, h = track_window 
+        #img2 = cv2.rectangle(frame, (x, y), (x + w*2, y + h*2), 255, 2) 
+        pts = cv2.boxPoints(ret)
+        pts = np.int0(pts)
+        img2 = cv2.polylines(frame,[pts],True, 255,2)
+          
+        # show results 
+        #cv2.imshow('tracker', img2)
        
     cv2.imwrite(filename='saved_img-final_MS.jpg', img=img2)
     
@@ -81,4 +83,3 @@ def Pretraitement():
       
     # destroy all opened windows 
     cv2.destroyAllWindows() 
-
